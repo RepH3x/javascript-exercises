1375a0525a350ae10245606c51317b8ffc575ec6
Samual Lewis <lewissamuala@gmail.com>
2024-06-16 21:34:58 -0700

    Complete 02: repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, timesToRepeat) {
+  let repeatedText = "";
+  if(timesToRepeat < 0) {
+    return "ERROR";
+  } else {
+    for(timesToRepeat; timesToRepeat > 0; timesToRepeat--) {
+      repeatedText += text;
+    }
+    return repeatedText;
+  }
 };
 
 // Do not edit below this line
