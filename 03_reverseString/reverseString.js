37ebb9cf755f6f6b4291e333f4e3c366cf9ef04c
Samual Lewis <lewissamuala@gmail.com>
2024-06-16 21:42:24 -0700

    Complete 03_reverseString Overcomplicated this one

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+  let stringArray = stringToReverse.split("");
+  let reversedString = "";
+  for(let i = stringArray.length - 1; i >= 0; i--) {
+    reversedString += stringArray[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
