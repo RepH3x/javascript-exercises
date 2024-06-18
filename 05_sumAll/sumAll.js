16f95f3164934fab52238c0344ffddfe55033e32
Samual Lewis <lewissamuala@gmail.com>
2024-06-17 20:24:44 -0700

    Complete 05: sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
 
+  if(firstNumber <= 0 || secondNumber <= 0 || typeof(firstNumber) != "number" 
+  || typeof(secondNumber) != "number") { 
+      return "ERROR";
+  }
+
+  if(firstNumber > secondNumber) {
+    let tempNumber = firstNumber 
+    firstNumber = secondNumber; 
+    secondNumber = tempNumber;
+  }
+
+  let finalNumber = 0;
+  while(firstNumber <= secondNumber) {
+    finalNumber += firstNumber;
+    firstNumber++;
+  }
+  return finalNumber;
 };
 
 // Do not edit below this line
