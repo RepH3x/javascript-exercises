6d98bcc94b38955301d43c0673220d05b8533ce6
Samual Lewis <lewissamuala@gmail.com>
2024-06-17 19:51:49 -0700

    Complete 04: removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  const args = Array.from(arguments);
+  const array = args.shift();
+  if (array == undefined || args.length === 0) {
+    console.log("Invalid input.");
+    return array;
+  } else {
+    return array.filter((key) => !args.includes(key))
+  }
 };
 
 // Do not edit below this line
