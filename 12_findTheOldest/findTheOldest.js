f764370e237a60c0d9deb6d9cb6cf7471dd79a02
Samual Lewis <lewissamuala@gmail.com>
2024-06-29 20:11:32 -0700

    Complete 12: Get The Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array
+    .reduce((list, person) => {
+      let deathYear = person["yearOfDeath"];
+      if (deathYear === undefined) person["yearOfDeath"] = new Date().getFullYear();
+      list.push(person);
+      return list;
+    }, [])
+    .sort((person, nextPerson) => {
+      let personAge = person["yearOfDeath"] - person["yearOfBirth"];
+      let nextPersonAge = nextPerson["yearOfDeath"] - nextPerson["yearOfBirth"];
+      return nextPersonAge - personAge;
+    })[0];
 };
 
 // Do not edit below this line
