b853e0a068cbb75872375b6e6d219efc5d576fe8
Samual Lewis <lewissamuala@gmail.com>
2024-06-29 18:22:13 -0700

    Complete 08: calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(operand, operator) {
+	return operand + operator;
 };
 
-const subtract = function() {
-	
+const subtract = function(operand, operator) {
+	return operand - operator;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, number) => total * number);
 };
 
-const power = function() {
-	
+const power = function(operand, operator) {
+	return Math.pow(operand, operator);
 };
 
-const factorial = function() {
-	
+const factorial = function(operand, operator) {
+  let total = 1;
+	for(let i=operand; i>=1; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
