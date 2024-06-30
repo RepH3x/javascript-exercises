0f730118af4b33891f8570c1e63cd4b8a4f68c53
Samual Lewis <lewissamuala@gmail.com>
2024-06-29 19:04:07 -0700

    Complete 09: palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str
+    .replace(/\W/ig, "")
+    .toLowerCase()
+    .split("");
+  let reverseStr = [...str].reverse();
+  for(let i=0; i<str.length; i++) {
+    if(str[i] !== reverseStr[i]) { return false; }
+  }
+  return true;
 };
 
 // Do not edit below this line
