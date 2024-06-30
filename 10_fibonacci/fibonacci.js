326ddb5ff0fe19640d7d575f5affd45dacecadb7
Samual Lewis <lewissamuala@gmail.com>
2024-06-29 19:24:24 -0700

    Complete 10: fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  const base = [0, 1];
+  if(index < 0) { return "OOPS"; }
+  if(index <= 1) { return base[index]; }
+  let generated = [...base];
+  for(let i=index; i >= base.length; i--) {
+    generated.push(generated.at(-1) + generated.at(-2));
+  }
+  return generated.at(-1);
 };
 
 // Do not edit below this line
